3f2920384d4507ef0673dc5c9406f9539e91552b
Marcin Żyżyński <mzyzynsk@mion.elka.pw.edu.pl>
2019-03-26 17:49:29 +0100

    Add project files

---
+++ README.md
@@ -0,0 +1,1 @@
+# system-zapisow-na-konsultacje-pik
---
+++ src/main/java/com/example/pikkonsultacje/PikKonsultacjeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pikkonsultacje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikKonsultacjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikKonsultacjeApplication.class, args);
+	}
+
+}
