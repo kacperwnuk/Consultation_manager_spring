c6f6780b07b4968ba27a015fdc6af01f84df43b9
zielinskis <szielin1@mion.elka.pw.edu.pl>
2019-05-15 22:33:59 +0200

    poprawione logowanie

--- angular/src/app/login/standard-login/standard-login.component.ts
+++ angular/src/app/login/standard-login/standard-login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { User } from '../model';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {Principal} from '../../rejestration/resource/principal';
 
 @Component({
     selector: 'app-standard-login',
@@ -9,6 +10,12 @@ import {Router} from '@angular/router';
     styleUrls: ['./standard-login.component.css']
 })
 export class StandardLoginComponent implements OnInit {
+
+  isActiveSpecialWord: boolean = false;
+  @Output() shift = new EventEmitter<boolean>();
+  username: string;
+  password: string;
+  user: Principal;
 
     constructor(
         private http: HttpClient,
@@ -18,29 +25,31 @@ export class StandardLoginComponent implements OnInit {
     ngOnInit() {
     }
 
-    isActiveSpecialWord: boolean = false;
-    @Output() shift = new EventEmitter<boolean>();
-    username: string;
-    password: string;
-    user: User;
-
     verification() {
         const url = 'https://localhost:8443/login';
-        this.user = new User();
+        this.user = new Principal();
         this.user.username = this.username;
         this.user.password = this.password;
-        this.user.enabled = false;
-        this.user.role = 1;
 
-        this.http.post<User>(url, this.user, {
-            observe: 'response',
-            responseType: 'json'
-          }).subscribe(response =>{
+
+        let headers = new HttpHeaders();
+        headers = headers.append('Authorization', 'Basic ' + btoa(`${this.user.username}:${this.user.password}`));
+        headers = headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+        const body = JSON.stringify({username: this.user.username, password: this.user.password});
+
+        this.http.get(url, {
+          headers: new HttpHeaders()
+            .set('Authorization', 'Basic ' + btoa(`${this.user.username}:${this.user.password}`)),
+          observe: 'response',
+          responseType: 'text'
+        }).subscribe(response =>{
             console.log(response);
-            this.router.navigate(['loged', response.body.username]);
+            alert('Logowanie powiodło się!');
+            //this.router.navigate(['loged', response.body.username]);
           },
           error =>{
-            alert("Logowanie nie powiodło się!");
+            alert('Logowanie nie powiodło się!');
             console.log(error);
           } );
     }
