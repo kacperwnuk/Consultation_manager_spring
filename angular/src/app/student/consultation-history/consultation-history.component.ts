ccc4e56a032a0819f28a7228ef94e82bc89bf7d8
Ja_łokim <Robimy_po_swojemu@tlen.pl>
2019-05-26 13:40:49 +0200

    dodanie nulowania konsultacji od strony studenta

--- angular/src/app/student/consultation-history/consultation-history.component.ts
+++ angular/src/app/student/consultation-history/consultation-history.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Principal} from "../../rejestration/resource/principal";
 import {ActivatedRoute, Router} from '@angular/router';
 import {Consultation} from '../resource/consultation';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { ResourceLoader } from '@angular/compiler';
 
 @Component({
   selector: 'app-consultation-history',
@@ -17,6 +18,10 @@ export class ConsultationHistoryComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.reload();
+  }
+
+  reload(){
     this.user = new Principal();
     this.user.username = this.route.snapshot.params['username'];
     this.user.password = this.route.snapshot.params['password'];
@@ -29,6 +34,30 @@ export class ConsultationHistoryComponent implements OnInit {
       console.log(this.collection);
       console.log(typeof this.collection[0].date);
     });
+  }
+
+  cancel(i) {
+    console.log("Zapisujesz sie na konsultacje nr " + i);
+    let params = new HttpParams();
+    params = params.set('consultationId', this.collection[i].id);
+    params = params.set('username', this.user.username);
+    const url = 'https://localhost:8443/cancelConsultation?' + params.toString();
+    console.log(url);
+
+    this.http.get(url, {
+      headers: new HttpHeaders()
+        .set('Authorization', 'Basic ' + btoa(`${this.user.username}:${this.user.password}`)),
+      observe: 'response',
+      responseType: 'text'
+    }).subscribe(response =>{
+        console.log(response);
+        this.reload();
+        alert('Anulowanie powiodło się!');
+      },
+      error =>{
+        alert('Anulowanie nie powiodło się');
+        console.log(error);
+      } );
   }
 
 }
